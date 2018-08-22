2896cb0ce338a4091310e47ca24c0552211e39c1
Amanda Wixted <amanda@amandawixted.com>
2018-08-22 16:39:45 -0400

    Initial Commit

---
+++ MazeWithView/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  MazeWithView
+//
+//  Created by Amanda Wixted on 8/22/18.
+//  Copyright © 2018 Meteor Grove Software. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
