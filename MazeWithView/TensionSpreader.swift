4b8f43309bf01bc60ea8368df94d54eeaeae4ef5
Amanda Wixted <amanda@amandawixted.com>
2018-08-31 13:06:50 -0400

    skip animation

--- MazeWithView/TensionSpreader.swift
+++ MazeWithView/TensionSpreader.swift
@@ -13,53 +13,63 @@ class TensionSpreader: VertexPlacer {
     private let repulsion_scalar: CGFloat = 100.0
     private let attraction_scalar: CGFloat = 0.060
     private let velocity_scalar: CGFloat = 0.8
-    private let avgNetForceThreshold: CGFloat = 0.0
+    private let avgNetForceThreshold: CGFloat = 0.01
+    private let animate = false
     
     func spreadOut(vertices: Set<VertexViewModel>) -> Bool {
         
-        var doneSpreadingOut: Bool = false
-        
-        // For each node in our graph, calculate its replusion from all the other nodes, and its attraction to the nodes it has a passage to
-        vertices.forEach { (v: VertexViewModel) in
+        func iterateSpreadOut() -> Bool {
             
-            v.netForce = .zero
+            var doneSpreadingOut = false
             
-            vertices.forEach { (u: VertexViewModel) in
-                guard u != v else { return }
+            // For each node in our graph, calculate its replusion from all the other nodes, and its attraction to the nodes it has a passage to
+            vertices.forEach { (v: VertexViewModel) in
                 
-                // squared distance between "u" and "v" in 2D space
-                let rsq = ((v.position.x-u.position.x)*(v.position.x-u.position.x)+(v.position.y-u.position.y)*(v.position.y-u.position.y))
+                v.netForce = .zero
                 
-                // make them repulse
-                v.netForce = v.netForce + (((v.position - u.position) / rsq) * repulsion_scalar)
+                vertices.forEach { (u: VertexViewModel) in
+                    guard u != v else { return }
+                    
+                    // squared distance between "u" and "v" in 2D space
+                    let rsq = ((v.position.x-u.position.x)*(v.position.x-u.position.x)+(v.position.y-u.position.y)*(v.position.y-u.position.y))
+                    
+                    // make them repulse
+                    v.netForce = v.netForce + (((v.position - u.position) / rsq) * repulsion_scalar)
+                }
+                // for each edge between our vertex in question and any other vertex, calculate the attraction between those two vertices
+                v.mazeNode.passages.forEach { (other: MazeNode) in
+                    guard v.mazeNode != other else { return }
+                    guard let u = vertices.filter({ $0.mazeNode == other }).first else { return }
+                    
+                    // make them attract since they're connected by a passage
+                    v.netForce = v.netForce + ((u.position - v.position) * attraction_scalar)
+                }
+                
+                v.velocity = (v.velocity + v.netForce) * velocity_scalar
             }
-            // for each edge between our vertex in question and any other vertex, calculate the attraction between those two vertices
-            v.mazeNode.passages.forEach { (other: MazeNode) in
-                guard v.mazeNode != other else { return }
-                guard let u = vertices.filter({ $0.mazeNode == other }).first else { return }
-                
-                // make them attract since they're connected by a passage
-                v.netForce = v.netForce + ((u.position - v.position) * attraction_scalar)
+            
+            let avgNetForce = vertices.reduce(0) { (value: CGFloat, v:VertexViewModel) -> CGFloat in
+                value + v.netForce.distanceFrom(.zero)
+                } / CGFloat(vertices.count)
+            
+            if avgNetForce < avgNetForceThreshold {
+                doneSpreadingOut = true
             }
             
-            v.velocity = (v.velocity + v.netForce) * velocity_scalar
+            // Now put the sprites in their new positions according to their netForces
+            vertices.forEach { (v: VertexViewModel) in
+                v.shape.position = v.velocity + v.shape.position
+            }
+            
+            return doneSpreadingOut
         }
         
-        let avgNetForce = vertices.reduce(0) { (value: CGFloat, v:VertexViewModel) -> CGFloat in
-            value + v.netForce.distanceFrom(.zero)
-            } / CGFloat(vertices.count)
-        
-        if avgNetForce < avgNetForceThreshold {
-            doneSpreadingOut = true
+        if !animate {
+            while !iterateSpreadOut() {}
+            return true
+        } else {
+            return iterateSpreadOut()
         }
         
-        // Now put the sprites in their new positions according to their netForces
-        vertices.forEach { (v: VertexViewModel) in
-            v.shape.position = v.velocity + v.shape.position
-        }
-        
-        print("avgNetForce: \(avgNetForce)")
-        
-        return doneSpreadingOut
     }
 }
